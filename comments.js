92e9653f30b3ce1682930e25f8c807cf472027df
Viktor Ponomarov <117126880+VVVektormanIO@users.noreply.github.com>
2023-12-18 00:00:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,63 @@
+// Create web server
+var express = require('express');
+var app = express();
+
+// Create a route for the app
+app.get("/", function(request, response) {
+  response.send("Welcome to my website!");
+});
+
+// Start the server
+app.listen(8080);
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+
+// Create a route for the app
+app.get("/", function(request, response) {
+  response.send("Welcome to my website!");
+});
+
+// Start the server
+app.listen(8080);
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+
+// Create a route for the app
+app.get("/", function(request, response) {
+  response.send("Welcome to my website!");
+});
+
+// Start the server
+app.listen(8080);
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+
+// Create a route for the app
+app.get("/", function(request, response) {
+  response.send("Welcome to my website!");
+});
+
+// Start the server
+app.listen(8080);
+
+// Path: comments.js
+// Create web server
+var express = require('express');
+var app = express();
+
+// Create a route for the app
+app.get("/", function(request, response) {
+  response.send("Welcome to my website!");
+});
+
+// Start the server
+app.listen(8080);
